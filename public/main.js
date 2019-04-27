286a20a4daa15e5748b205bde26c44dd2beea774
Aaron Lamoreaux <LamoreauxAJ@gmail.com>
2019-04-27 18:58:03 -0500

    renamed views

--- public/main.js
+++ public/main.js
@@ -2,6 +2,7 @@ import HelloWorld from './components/HelloWorld.js'
 
 let vue = new Vue({
   el: '#app',
+  components: { HelloWorld },
   template: `
     <div>
       <h1> {{ msg }}! </h1>
--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/../views/index.html'))
 })
 
-app.use(express.static('views'))
+app.use(express.static('public'))
 
 app.listen(config.port, () => console.log(`Listening on port ${config.port}!`))
