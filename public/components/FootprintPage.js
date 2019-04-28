b3acada120aae5c668cde62d6bcf086e7fdc4c52
Ali Pervaiz <syedaliakrampervaiz@gmail.com>
2019-04-28 00:18:01 -0500

    Made footprint page and started on leaderboard

---
+++ public/components/FootprintPage.js
@@ -0,0 +1,11 @@
+import Layout from './Layout.js'
+
+export default {
+  name: 'FootprintPage',
+  components: {Layout},
+  template: `
+    <Layout>
+      Hello
+    </Layout>
+  `
+}
--- public/components/RankPage.js
+++ public/components/RankPage.js
@@ -4,8 +4,24 @@ export default {
   name: 'RankPage',
   components: { Layout },
   template: `
-    <Layout>
-      This is a graph!
-    </Layout>
+  	<Layout>
+		<div class="leaderboard">
+		<div class = "overlay">
+		   <div class = "detail">
+		     <span class = "name">Leaderboard</span>
+		     <span class = "sub">
+		       <span class = "bold">1</span>
+		     </span>
+		   </div>
+		 </div>
+		   <div class="row">
+		     <div class="icon"></div>
+		     <span class="avatar"></span>
+		     <span class="name">Randy Goldman</span>
+		     <span class="points">0</span>
+		   </div>
+		 </div>
+		</div>   
+    </Layout>  
   `
 }
