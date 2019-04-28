3a1849d257409f286ac14b191aa9747789c29faa
Ali Pervaiz <syedaliakrampervaiz@gmail.com>
2019-04-28 00:57:49 -0500

    rank stuff

--- public/components/RankPage.js
+++ public/components/RankPage.js
@@ -6,21 +6,44 @@ export default {
   template: `
   	<Layout>
 		<div class="leaderboard">
-		<div class = "overlay">
-		   <div class = "detail">
-		     <span class = "name">Leaderboard</span>
-		     <span class = "sub">
-		       <span class = "bold">1</span>
-		     </span>
-		   </div>
-		 </div>
-		   <div class="row">
-		     <div class="icon"></div>
-		     <span class="avatar"></span>
-		     <span class="name">Randy Goldman</span>
-		     <span class="points">0</span>
-		   </div>
-		 </div>
+			<div class = "overlay">
+			   <div class = "detail">
+			    	<span class = "name">Leaderboard</span>
+			     	<span class = "sub">
+			       		<span class = "bold">
+			       			<span>#24 in Dallas</span>
+			       		</span>
+			     	</span>
+			   	</div>
+			   	<a href="/#/rank" class="button selected"><i class="fas fa-map-marker"></i></a>
+			   	<a href="/#/rank" class="button"><i class="fas fa-flag"></i></a>
+			   	<a href="/#/rank" class="button"><i class="fas fa-globe"></i></a>
+			</div>
+			 	<div class="row">
+			     <span class="icon"><i class="fas fa-trophy"></i></span>
+			     <span class="avatar"></span>
+			     <span class="name">Randy Goldman</span>
+			     <span class="points">0</span>
+			   </div>
+			   <div class="row">
+			     <span class="icon"><i class="fas fa-trophy"></i></span>
+			     <span class="avatar"></span>
+			     <span class="name">Randy Goldman</span>
+			     <span class="points">0</span>
+			   </div>
+			   <div class="row">
+			     <span class="icon"><i class="fas fa-trophy"></i></span>
+			     <span class="avatar"></span>
+			     <span class="name">Randy Goldman</span>
+			     <span class="points">0</span>
+			   </div>
+			   <div class="row">
+			     <span class="icon">4</span>
+			     <span class="avatar"></span>
+			     <span class="name">Randy Goldman</span>
+			     <span class="points">0</span>
+			   </div>
+			</div>
 		</div>   
     </Layout>  
   `
