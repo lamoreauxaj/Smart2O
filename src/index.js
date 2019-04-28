27ea01686a565a26d9d046d0d340c4291b9688dd
Sinister-Paradox <alexdejong737@gmail.com>
2019-04-28 08:45:44 -0500

    get

--- src/index.js
+++ src/index.js
@@ -26,15 +26,18 @@ app.get('/loginauth', function(req, res){
 
 // GET /user/wateruse
 app.get('/:user/wateruse', function(req, res){
-	res.json({"name": req.params.user,
+    if(req.params.user == 'syedpervaiz'){
+        res.json({"name": req.params.user,
+            "profilepic": "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-1/p160x160/38462817_2145135052393744_2504584752456007680_n.jpg?_nc_cat=100&_nc_ht=scontent-dfw5-2.xx&oh=761876450882fff99b0e585e1a610c3f&oe=5D72490F",
 			"friends": ["flexadecimal","aaronl"],
+            "score":3,
 			"totalUse": 100.3,
 			"waterBillPrice":40.81,
-			"gallonsPerDay":
-				{"showers":30.1, "washing_machine":10.0, "toilet":24.1,
-				"dishwashers":4.3, "baths":0.0, "leaks":4.0,"faucets":10.9,"other": 16.9}
+			"gallonsPercents":
+				{"showers":52, "washing_machine":10.0, "toilet":12,
+				"dishwashers":3,"faucets":21,"other": 12}
 			}
-	)
+	)}
 });
 
 // GET /user/game
